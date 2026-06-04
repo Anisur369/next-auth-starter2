e16d581ae4c5680d807a20686946837f9f459229
anisur <anisur@anisur.anisur>
2026-06-04 09:59:12 +0600

    nextauth LoginButton

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,10 @@
+"use client";
+import { signIn } from "next-auth/react"
+
+const LoginButton = () => {
+    return <button className="btn" onClick={() => signIn()}>
+        Login Now
+    </button>
+}
+
+export default LoginButton
