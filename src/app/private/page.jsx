7ff1cbce3eb9330227bc9b71f2a11db2dc3dcdf5
anisur <anisur@anisur.anisur>
2026-06-11 12:22:12 -0400

    update private page

--- src/app/private/page.jsx
+++ src/app/private/page.jsx
@@ -3,6 +3,7 @@ const PrivatePage = () => {
         <div className="min-h-screen min-w-screen relative flex flex-col justify-center text-center mx-auto max-w-7xl gap-5">
             <h1 className="text-4xl font-bold">Private Page</h1>
             <p>This is a simple private page.</p>
+            <p>Only authenticated users can access this page.</p>
         </div>
     );
 }
